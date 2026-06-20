28b919b47a5378af6e8f2d1f95cddbf7ab15bd47
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-20 17:04:12 +0300

    Update home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,27 @@
+import { useState } from 'react';
 import MainScene from '../components/MainScene';
+import UIOverlay from '../components/UIOverlay';
 import Welcome from '../components/Welcome';
 import AtharOfDay from '../components/AtharOfDay';
 import PrayerTimes from '../components/PrayerTimes';
 import QiblaFinder from '../components/QiblaFinder';
-import ShareButton from '../components/ShareButton';
-import NameModal from '../components/NameModal';
 import Footer from '../components/Footer';
-import UIOverlay from '../components/UIOverlay';
+import NameModal from '../components/NameModal';
 
 export default function Home() {
+  const [showNameModal, setShowNameModal] = useState(false);
+
   return (
-    <>
+    <div className="relative w-screen h-screen overflow-hidden bg-black">
       <MainScene />
-      <Welcome />
-      <AtharOfDay />
-      <PrayerTimes />
-      <QiblaFinder />
-      <ShareButton />
-      <NameModal />
-      <Footer />
-      <UIOverlay />
-    </>
+      <UIOverlay>
+        <Welcome onNameClick={() => setShowNameModal(true)} />
+        <AtharOfDay />
+        <PrayerTimes />
+        <QiblaFinder />
+        <Footer />
+      </UIOverlay>
+      {showNameModal && <NameModal onClose={() => setShowNameModal(false)} />}
+    </div>
   );
 }
