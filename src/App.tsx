2a77d2c7af0de6857c315a844e5ebed06d3d7848
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-23 02:31:00 +0300

    feat: add page transitions with Framer Motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import BottomNav from "./components/Navigation/BottomNav";
+import PageTransition from "./components/Shared/PageTransition";
 import HomePage from "./pages/HomePage";
 import DhikrPage from "./pages/DhikrPage";
 import QuranPage from "./pages/QuranPage";
@@ -11,9 +12,30 @@ export default function App() {
       <div className="min-h-screen bg-gradient-to-b from-[#E8EDF2] to-white font-arabic pb-20">
         <div className="max-w-md mx-auto p-4">
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/dhikr" element={<DhikrPage />} />
-            <Route path="/quran" element={<QuranPage />} />
+            <Route
+              path="/"
+              element={
+                <PageTransition>
+                  <HomePage />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/dhikr"
+              element={
+                <PageTransition>
+                  <DhikrPage />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/quran"
+              element={
+                <PageTransition>
+                  <QuranPage />
+                </PageTransition>
+              }
+            />
           </Routes>
         </div>
         <BottomNav />
