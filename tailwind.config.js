08d2c372288857d82c75fcf45edbc38201cd2dbc
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-23 00:20:26 +0300

    config: add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-bg': '#F5F0E8',
+        'card-bg': '#FFFFFF',
+        'primary-text': '#3E4A3C',
+        'secondary-text': '#8A8A8A',
+        'accent': '#5B6E5A',
+        'highlight': '#A67C52',
+        'action': '#3E4A3C',
+      },
+      borderRadius: {
+        'card': '20px',
+      },
+      fontFamily: {
+        'arabic': ['Cairo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
