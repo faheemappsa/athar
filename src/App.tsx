7c697e554602a5cf49c19c28deb6bc2a4c7c1a68
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-23 16:00:02 +0300

    fix: wrap app with error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import HomePage from "./pages/HomePage";
 import DhikrPage from "./pages/DhikrPage";
 import QuranPage from "./pages/QuranPage";
 import PageTransition from "./components/Shared/PageTransition";
+import ErrorBoundary from "./components/Shared/ErrorBoundary";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen w-full bg-primary-bg font-arabic flex flex-col">
-        <div className="flex-1 w-full max-w-md mx-auto px-4 pb-28 pt-4">
-          <Routes>
-            <Route path="/" element={<PageTransition><HomePage /></PageTransition>} />
-            <Route path="/dhikr" element={<PageTransition><DhikrPage /></PageTransition>} />
-            <Route path="/quran" element={<PageTransition><QuranPage /></PageTransition>} />
-          </Routes>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <div className="min-h-screen w-full bg-primary-bg font-arabic flex flex-col">
+          <div className="flex-1 w-full max-w-md mx-auto px-4 pb-28 pt-4">
+            <Routes>
+              <Route path="/" element={<PageTransition><HomePage /></PageTransition>} />
+              <Route path="/dhikr" element={<PageTransition><DhikrPage /></PageTransition>} />
+              <Route path="/quran" element={<PageTransition><QuranPage /></PageTransition>} />
+            </Routes>
+          </div>
+          <BottomNav />
         </div>
-        <BottomNav />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
