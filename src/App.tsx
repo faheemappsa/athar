5985aec726f27f77548f01733cf595b9552489b5
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-23 03:07:27 +0300

    fix: center mobile app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import BottomNav from "./components/Navigation/BottomNav";
-import PageTransition from "./components/Shared/PageTransition";
 import HomePage from "./pages/HomePage";
 import DhikrPage from "./pages/DhikrPage";
 import QuranPage from "./pages/QuranPage";
+import PageTransition from "./components/Shared/PageTransition";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gradient-to-b from-[#E8EDF2] to-white font-arabic pb-20">
-        <div className="max-w-md mx-auto p-4">
+      <div className="min-h-screen bg-gradient-to-b from-[#F0F4F8] to-white font-arabic flex flex-col">
+        <div className="flex-1 max-w-md w-full mx-auto px-4 pb-20 pt-4">
           <Routes>
-            <Route
-              path="/"
-              element={
-                <PageTransition>
-                  <HomePage />
-                </PageTransition>
-              }
-            />
-            <Route
-              path="/dhikr"
-              element={
-                <PageTransition>
-                  <DhikrPage />
-                </PageTransition>
-              }
-            />
-            <Route
-              path="/quran"
-              element={
-                <PageTransition>
-                  <QuranPage />
-                </PageTransition>
-              }
-            />
+            <Route path="/" element={<PageTransition><HomePage /></PageTransition>} />
+            <Route path="/dhikr" element={<PageTransition><DhikrPage /></PageTransition>} />
+            <Route path="/quran" element={<PageTransition><QuranPage /></PageTransition>} />
           </Routes>
         </div>
         <BottomNav />
