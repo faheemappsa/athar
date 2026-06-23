16d8d78fb14461129d71d0ceb742622ab640f24e
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-23 09:31:35 +0300

    style page spacing

--- src/pages/DhikrPage.tsx
+++ src/pages/DhikrPage.tsx
@@ -3,8 +3,8 @@ import Footer from "../components/Footer/Footer";
 
 export default function DhikrPage() {
   return (
-    <div className="space-y-6">
-      <h2 className="text-2xl font-bold text-center text-[#1A3B5C]">الأذكار</h2>
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold text-center text-primary-text">الأذكار</h2>
       <Dhikr />
       <Footer />
     </div>
