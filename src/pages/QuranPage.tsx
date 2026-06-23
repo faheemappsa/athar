a8e894c831a6966548ee6eb7324b1d3d23e2f080
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-23 09:32:04 +0300

    style page spacing

--- src/pages/QuranPage.tsx
+++ src/pages/QuranPage.tsx
@@ -3,8 +3,8 @@ import Footer from "../components/Footer/Footer";
 
 export default function QuranPage() {
   return (
-    <div className="space-y-6">
-      <h2 className="text-2xl font-bold text-center text-[#1A3B5C]">المصحف</h2>
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold text-center text-primary-text">المصحف</h2>
       <Quran />
       <Footer />
     </div>
