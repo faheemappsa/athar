780c29346b63d0c9e41610ad061d1fc57ba238a7
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-26 06:05:06 +0300

    refactor: remove home hero card

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,12 +2,10 @@ import PrayerTimes from "../components/PrayerTimes/PrayerTimes";
 import RadioPlayer from "../components/Radio/RadioPlayer";
 import AtharCard from "../components/AtharCard/AtharCard";
 import Footer from "../components/Footer/Footer";
-import AppHero from "../components/Shared/AppHero";
 
 export default function HomePage() {
   return (
     <div className="space-y-5">
-      <AppHero title="أثر" subtitle="رفيق يومي للأذكار وورد القرآن ومشاركة الأثر" />
       <PrayerTimes />
       <RadioPlayer />
       <AtharCard />
