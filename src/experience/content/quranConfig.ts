067d923bf6d813c97ea037c5ae01173e31de8378
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-28 07:05:14 +0300

    feat(content): add quran provider config

---
+++ src/experience/content/quranConfig.ts
@@ -0,0 +1,5 @@
+export const QURAN_COM_API_BASE = "https://api.quran.com/api/v4";
+
+export const ATHAR_SHORT_VERSES = ["94:5", "94:6", "65:3", "13:28", "2:286", "93:5"];
+
+export const ATHAR_TAFSIR_ID = 16;
