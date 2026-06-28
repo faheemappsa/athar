9aa4de3f1807ce88443687af62967a9f9d04aaba
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-28 08:26:20 +0300

    fix(athar): refresh card on home navigation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,17 @@
+import { useLocation } from "react-router-dom";
 import PrayerTimes from "../components/PrayerTimes/PrayerTimes";
 import RadioPlayer from "../components/Radio/RadioPlayer";
 import AtharCard from "../components/AtharCard/AtharCard";
 import Footer from "../components/Footer/Footer";
 
 export default function HomePage() {
+  const location = useLocation();
+
   return (
     <div className="space-y-5">
       <PrayerTimes />
       <RadioPlayer />
-      <AtharCard />
+      <AtharCard key={location.key} />
       <Footer showWaqf />
     </div>
   );
