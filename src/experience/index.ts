776d24d3b29be3971252cbc9d86bc4ef4266974a
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-06-28 07:14:15 +0300

    feat(experience): add experience barrel

---
+++ src/experience/index.ts
@@ -0,0 +1,6 @@
+export { resolveDecision } from "./resolveDecision";
+export { resolveContent } from "./resolveContent";
+export { runAtharBrain, readLastAtharBrainDecision } from "./brain";
+export { recordAtharBehavior } from "./memory";
+export { useSurfaceSignals } from "./useSurfaceSignals";
+export type { AtharBrainDecision, AtharBehaviorEvent, AtharSurface } from "./types";
