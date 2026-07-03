7eb50d7f4479b537454b2caffae7d307d2b394b4
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-07-03 16:18:05 +0300

    feat(athar-v2): expose isolated public API

---
+++ src/athar-v2/index.ts
@@ -0,0 +1,15 @@
+export { ATHAR_V2_LIBRARY } from "./library";
+export { createAtharV2Moment } from "./moment";
+export { selectAtharV2, getAtharV2LibrarySnapshot } from "./picker";
+export { validateAtharV2Library } from "./validate";
+export type {
+  AtharV2ContentType,
+  AtharV2Item,
+  AtharV2Moment,
+  AtharV2MomentInput,
+  AtharV2Occasion,
+  AtharV2SelectionOptions,
+  AtharV2SelectionResult,
+  AtharV2Source,
+  AtharV2Tag,
+} from "./types";
