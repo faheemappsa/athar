073012c82c57aff1793ace61aa38985f2013f532
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-07-03 16:19:01 +0300

    feat(athar-v2): export scenario helpers

--- src/athar-v2/index.ts
+++ src/athar-v2/index.ts
@@ -1,6 +1,7 @@
 export { ATHAR_V2_LIBRARY } from "./library";
 export { createAtharV2Moment } from "./moment";
 export { selectAtharV2, getAtharV2LibrarySnapshot } from "./picker";
+export { ATHAR_V2_SCENARIO_DATES, getAtharV2ScenarioSamples } from "./scenarios";
 export { validateAtharV2Library } from "./validate";
 export type {
   AtharV2ContentType,
