a377151e5c7d234a31bc8d96e54af42510a7aaf3
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-07-03 16:21:32 +0300

    feat(athar-v2): export service and memory helpers

--- src/athar-v2/index.ts
+++ src/athar-v2/index.ts
@@ -1,7 +1,9 @@
 export { ATHAR_V2_LIBRARY } from "./library";
+export { normalizeAtharV2RecentIds } from "./memory";
 export { createAtharV2Moment } from "./moment";
 export { selectAtharV2, getAtharV2LibrarySnapshot } from "./picker";
 export { ATHAR_V2_SCENARIO_DATES, getAtharV2ScenarioSamples } from "./scenarios";
+export { getAtharV2ForMoment } from "./service";
 export { validateAtharV2Library } from "./validate";
 export type {
   AtharV2ContentType,
