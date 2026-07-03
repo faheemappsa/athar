6b3618501241bab14bdd17ce9788f5483532d125
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-07-03 17:33:20 +0300

    feat(athar-v2): export card runtime API

--- src/athar-v2/index.ts
+++ src/athar-v2/index.ts
@@ -1,11 +1,18 @@
 export { ATHAR_V2_AYAH_LIBRARY } from "./ayahLibrary";
+export { getAtharV2CardContent } from "./cardService";
 export { ATHAR_V2_CATALOG } from "./catalog";
+export { toAtharV2CardContent } from "./compat";
+export type { AtharV2CardContent } from "./compat";
 export { ATHAR_V2_DUA_LIBRARY } from "./duaLibrary";
+export { isAtharV2Enabled, setAtharV2Enabled } from "./featureFlag";
 export { ATHAR_V2_LIBRARY } from "./library";
 export { ATHAR_V2_MEANING_LIBRARY } from "./meaningLibrary";
 export { normalizeAtharV2RecentIds } from "./memory";
 export { createAtharV2Moment } from "./moment";
 export { selectAtharV2, getAtharV2LibrarySnapshot } from "./picker";
+export type { AtharV2RecentStore } from "./recentStore";
+export { createAtharV2RecentStore } from "./recentStore";
+export { runAtharV2Card } from "./runtime";
 export { ATHAR_V2_SCENARIO_DATES, getAtharV2ScenarioSamples } from "./scenarios";
 export { getAtharV2ForMoment } from "./service";
 export { runAtharV2SelfTest } from "./selfTest";
