b373fd141e2f07d889ef5bfd817af78e8e60fc99
Faheem | فهيم <cxm.abdulaziz@gmail.com>
2026-07-03 17:59:36 +0300

    feat(athar-v2): include final content pack in catalog

--- src/athar-v2/catalog.ts
+++ src/athar-v2/catalog.ts
@@ -1,4 +1,5 @@
 import { ATHAR_V2_AYAH_LIBRARY } from "./ayahLibrary";
+import { ATHAR_V2_CONTENT_PACK_FINAL } from "./contentPackFinal";
 import { ATHAR_V2_DUA_LIBRARY } from "./duaLibrary";
 import { ATHAR_V2_LIBRARY } from "./library";
 import { ATHAR_V2_MEANING_LIBRARY } from "./meaningLibrary";
@@ -9,4 +10,5 @@ export const ATHAR_V2_CATALOG: AtharV2Item[] = [
   ...ATHAR_V2_AYAH_LIBRARY,
   ...ATHAR_V2_DUA_LIBRARY,
   ...ATHAR_V2_MEANING_LIBRARY,
+  ...ATHAR_V2_CONTENT_PACK_FINAL,
 ];
